1662fbfd059fd04b5e391e5e165e8ae57b7ea501
David Doyle-Owen <thinkimagine@outlook.com>
2025-06-06 21:56:34 +0100

    Update database url and secure secret key

--- tolkienforum/settings.py
+++ tolkienforum/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cu2e7anj64y7t40oc=$^088!n668=5%6i!8=u#)m*jcns_s32*'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
