02b2377e7760524c244a4a1ab47a178468b46b28
David Doyle-Owen <thinkimagine@outlook.com>
2025-06-11 14:17:06 +0100

    Add related names in comment model

--- forum/models.py
+++ forum/models.py
@@ -29,14 +29,15 @@ class Article(models.Model):
 
     # Enable more user friendly article naming
     def __str__(self):
-        return f"{self.title} | Created by {self.author}"
+        return f"{self.title} | created by {self.author}"
 
 
 # Modelled on walkthrough Django-Blog, modified
 class Comment(models.Model):
     article = models.ForeignKey(
         Article, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='commenter')
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
