2bd7461be51363660d5b49a0bb55465f86d46817
David Doyle-Owen <thinkimagine@outlook.com>
2025-06-11 13:34:30 +0100

    Add comment model

--- forum/models.py
+++ forum/models.py
@@ -9,7 +9,7 @@ STATUS = (
 
 
 # Create your models here.
-# Modelled on walkthrough Django-Blog
+# Modelled on walkthrough Django-Blog, modified
 class Article(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -21,4 +21,14 @@ class Article(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     updated_on = models.DateTimeField(auto_now=True)
     exerpt = models.TextField(blank=True)
+    approved = models.BooleanField(default=False)
+
+
+# Modelled on walkthrough Django-Blog, modified
+class Comment(models.Model):
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
