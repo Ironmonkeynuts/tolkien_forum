bbc3d5cff1331ce6404adaf5ee87dedebea471eb
David Doyle-Owen <thinkimagine@outlook.com>
2025-06-14 11:42:32 +0100

    Add created_on to filter. Load data to fixtures folder in forum

--- forum/admin.py
+++ forum/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Article, Comment
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Article, Comment
 
 
 @admin.register(Article)  # Decorator register
@@ -8,10 +8,11 @@ class ArticleAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on', 'approved')
     # Displayed fields
     search_fields = ['title', 'content']  # Searchable fields
-    list_filter = ("status", "approved",)  # Filterable fields
-    prepopulated_fields = {"slug": ["title",]}  # Automatically filled fields
+    list_filter = ("status", "created_on", "approved",)  # Filterable fields
+    prepopulated_fields = {"slug": ("title",)}  # Automatically filled fields
     summernote_fields = ("content",)  # Summernote fields
 
 
 # Register your models here.
+# admin.site.register(Article)
 admin.site.register(Comment)
