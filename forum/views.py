08359ee1ff455b88724ab2f2904aa33984593839
David Doyle-Owen <thinkimagine@outlook.com>
2025-06-15 20:11:22 +0100

    Add templates to views

--- forum/views.py
+++ forum/views.py
@@ -8,5 +8,19 @@ class ArticleList(generic.ListView):
     """
     A view that displays a list of articles.
     """
-    queryset = Article.objects.filter(status=1)
-    template_name = 'article_list.html'
+    queryset = Article.objects.filter(status=1, approved=True)
+    template_name = 'forum/forum.html'
+    paginate_by = 3
+
+
+def welcome(request):
+    return render(request, 'forum/welcome.html')
+
+def profile(request):
+    return render(request, 'forum/profile.html')
+
+def about(request):
+    return render(request, 'forum/about.html')
+
+def contact(request):
+    return render(request, 'forum/contact.html')
