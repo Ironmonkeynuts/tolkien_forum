d12eb81fa2f1b96e95a55d779c7df2b6761f4009
David Doyle-Owen <thinkimagine@outlook.com>
2025-06-16 10:59:35 +0100

    Register Comment and Profile in admin

--- forum/admin.py
+++ forum/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Article, Comment
+from .models import Article, Comment, Profile
 
 
 @admin.register(Article)  # Decorator register
 class ArticleAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on', 'approved')
     # Displayed fields
-    search_fields = ['title', 'content']  # Searchable fields
+    search_fields = ['title', 'content', 'author__username']  # Searchable fields
     list_filter = ("status", "created_on", "approved",)  # Filterable fields
     prepopulated_fields = {"slug": ("title",)}  # Automatically filled fields
     summernote_fields = ("content",)  # Summernote fields
@@ -15,4 +15,24 @@ class ArticleAdmin(SummernoteModelAdmin):
 
 # Register your models here.
 # admin.site.register(Article)
-admin.site.register(Comment)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('body', 'article', 'author', 'created_on', 'approved')
+    # Displayed fields
+    search_fields = ('body', 'author__username')  # Searchable fields
+    list_filter = ('created_on', 'approved')  # Filterable fields
+
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+    approve_comments.short_description = "Approve selected comments"
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'user_type', 'approved')
+    # Displayed fields
+    search_fields = ('user__username', 'user_type')  # Searchable fields
+    list_filter = ('user_type', 'approved')  # Filterable fields
