e019d146ea9b599e8de106a32348920407c92efc
David Doyle-Owen <thinkimagine@outlook.com>
2025-06-16 12:04:53 +0100

    Create basic article_detail template and add class to views

--- forum/urls.py
+++ forum/urls.py
@@ -5,6 +5,7 @@ from .views import ArticleList
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('forum/', ArticleList.as_view(), name='forum'),
+    path('forum/<slug:slug>/', views.ArticleDetail.as_view(), name='article_detail'),
     path('profile/', views.profile, name='profile'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
--- forum/views.py
+++ forum/views.py
@@ -10,17 +10,35 @@ class ArticleList(generic.ListView):
     """
     queryset = Article.objects.filter(status=1, approved=True)
     template_name = 'forum/forum.html'
-    paginate_by = 3
+    paginate_by = 6
+
+class ArticleDetail(generic.DetailView):
+    """
+    A view that displays the details of a specific article.
+    """
+    model = Article
+    template_name = 'forum/article_detail.html'
+    context_object_name = 'article'
+
+    # Filter out drafts/unapproved articles for non-staff users
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.request.user.is_staff or self.request.user.is_superuser:
+            return qs
+        return qs.filter(status=1, approved=True)
 
 
 def welcome(request):
     return render(request, 'forum/welcome.html')
 
+
 def profile(request):
     return render(request, 'forum/profile.html')
 
+
 def about(request):
     return render(request, 'forum/about.html')
+
 
 def contact(request):
     return render(request, 'forum/contact.html')
