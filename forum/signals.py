b0747a7fda3d13d4db50d25cf56da42c381b1e32
David Doyle-Owen <thinkimagine@outlook.com>
2025-06-16 11:22:45 +0100

    Add comments to signals.py

--- forum/signals.py
+++ forum/signals.py
@@ -3,7 +3,12 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
 
+# Listens for the post-save signal from User model
+# When a user model is created or updated the function runs automatically
+
+# Connects function create_or_update_profile to run after any User is saved
 @receiver(post_save, sender=User)
+# Checks instance of user is already created
 def create_or_update_profile(sender, instance, created, **kwargs):
     if created:
         # If new user, create profile
