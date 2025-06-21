6a2efabc94d0ef6c081437771c2f0383478eac8f
David Doyle-Owen <thinkimagine@outlook.com>
2025-06-21 12:08:13 +0100

    Add article form template  add class to forms

--- forum/forms.py
+++ forum/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Comment
+from django_summernote.widgets import SummernoteWidget
+from .models import Comment, Article
 
 
 class CommentForm(forms.ModelForm):
@@ -11,4 +12,16 @@ class CommentForm(forms.ModelForm):
                 'class': 'form-control',
                 'rows': 4,
                 'placeholder': 'Your comment...'}),
+        }
+
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'excerpt', 'content', 'status']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'excerpt': forms.Textarea(attrs={'class': 'form-control', 'rows': 2}),
+            'content': SummernoteWidget(),
+            'status': forms.Select(attrs={'class': 'form-select'}),
         }
