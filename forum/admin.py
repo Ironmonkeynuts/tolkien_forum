483869454f1dd100fb38d5cd2cd17cd93a0c9b97
David Doyle-Owen <thinkimagine@outlook.com>
2025-07-01 12:15:42 +0100

    Add admin views for Contact message, Creator Application and moderator Application

--- forum/admin.py
+++ forum/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Article, Comment, Profile
+from .models import Article, Comment, Profile, ContactMessage, CreatorApplication, ModeratorApplication
 
 
 @admin.register(Article)  # Decorator register
@@ -33,6 +33,46 @@ class CommentAdmin(admin.ModelAdmin):
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'user_type', 'approved')
-    # Displayed fields
+    search_fields = ('user__username', 'user_type')
+    list_filter = ('user_type', 'approved')
+
+    # Make user_type editable in the form
+    fields = ('user', 'user_type', 'avatar', 'bio', 'approved')
+
+    # Restrict editing to prevent changes to already approved users
+    def get_readonly_fields(self, request, obj=None):
+        if obj and obj.approved:
+            return ['user_type']  # Prevent editing user_type after approval
+        return []    # Displayed fields
     search_fields = ('user__username', 'user_type')  # Searchable fields
     list_filter = ('user_type', 'approved')  # Filterable fields
+
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('email', 'created_on')
+    search_fields = ('email', 'message')
+
+
+@admin.register(CreatorApplication)
+class CreatorApplicationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_on', 'approved', 'reviewed')
+    search_fields = ('user__username', 'reason')
+    list_filter = ('approved', 'reviewed')
+    actions = ['approve_selected']
+
+    def approve_selected(self, request, queryset):
+        queryset.update(approved=True)
+    approve_selected.short_description = "Approve selected applications"
+
+
+@admin.register(ModeratorApplication)
+class ModeratorApplicationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_on', 'approved', 'reviewed')
+    search_fields = ('user__username', 'reason')
+    list_filter = ('approved', 'reviewed')
+    actions = ['approve_selected']
+
+    def approve_selected(self, request, queryset):
+        queryset.update(approved=True)
+    approve_selected.short_description = "Approve selected applications"
