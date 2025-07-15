7e09aad1f9a13c403ed4f4b25d03955aef9bdf6b
David Doyle-Owen <thinkimagine@outlook.com>
2025-07-15 12:16:44 +0100

    Remove commented out code from settings

--- tolkienforum/settings.py
+++ tolkienforum/settings.py
@@ -26,9 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
@@ -104,15 +101,6 @@ CSRF_TRUSTED_ORIGINS = [
 
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-# Uncomment below for production email via SMTP
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.sendgrid.net'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
-# EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
-# DEFAULT_FROM_EMAIL = 'Your Site <noreply@yoursite.com>'
 
 
 # Password validation
